846e0a9b9607d4047244b5c8366e8b2aa61d85fe
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-01-17 11:33:10 +0100

    Adding and testing data documentation

--- other/play.R
+++ other/play.R
@@ -12,3 +12,7 @@ df_h_1armcluster(N_total = 150, ICC = 0.1, N_grp = 100, n_clusters = 10)
 df_h(N_total = c(100), ICC = 0.1, avg_grp_size = 10)
 
 df_puste(N_total = c(100, 50), ICC = 0.1, n_clusters = 10)
+
+VIVECampbell::Dietrichson2021_data
+
+dplyr::glimpse(Dietrichson2021_data)
