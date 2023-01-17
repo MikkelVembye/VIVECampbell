5045a8c35982e14952e6ef36c8eebd61db6d5796
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-01-17 20:15:42 +0100

    Testing new df_h function

--- other/play.R
+++ other/play.R
@@ -9,7 +9,7 @@ load_all()
 #
 df_h_1armcluster(N_total = 100, ICC = 0.1, N_grp = 60, avg_grp_size = 5)
 
-df_h(N_total = c(100), ICC = 0.1, avg_grp_size = 10)
+df_h(N_total = c(100), ICC = 0.1, avg_grp_size = 10, n_clusters = 10, df_type = "Pustejovsky")
 
 
 VIVECampbell::Dietrichson2021_data
