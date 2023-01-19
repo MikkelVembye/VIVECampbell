abb156fc11c940899703ea3f71048a90d8b242a4
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-01-19 10:53:23 +0100

    Testing df change in df_h_1armcluster()

--- other/play.R
+++ other/play.R
@@ -17,3 +17,23 @@ VIVECampbell::Dietrichson2021_data
 dplyr::glimpse(Dietrichson2021_data)
 
 use_vignette("VIVECampbell")
+
+# Test change
+
+
+df <- df_h_1armcluster(N_total = 100, ICC = 0.1, N_grp = 60, avg_grp_size = 5)
+
+# Raw calculation
+N <- 100
+rho <- 0.1
+NT <- 60
+n <- 5
+
+raw_calc <- ((N-2)*(1-rho) + (NT-n)*rho)^2 /
+  ( (N-2)*(1-rho)^2 + (NT-n)*n*rho^2 + 2*(NT-n)*(1-rho)*rho )
+
+df == round(raw_calc, 2)
+
+
+
+
